affd596de5fcd49eeb14ee98d65fb9feb102085a
whg <whg3.3.3@163.com>
2016-05-24 15:50:37 +0800

    update

--- backend/java/src/main/java/SocketIORedissonServer.java
+++ backend/java/src/main/java/SocketIORedissonServer.java
@@ -25,25 +25,25 @@ import com.corundumstudio.socketio.store.pubsub.PubSubStore;
  * 与{@link SocketIOProtoServer}类似，只是这里添加了redis（redisson封装的）作为存储客户端数据的地方，而不是默认的内存存储（虽然内存存储啥也没做）
  */
 public class SocketIORedissonServer implements ConnectListener, DisconnectListener, PubSubListener<ConnectMessage>{
-	
-	private static final String HOST = "localhost";
-	private static final int PORT = 3001;
-	
-	//单机模式
-	private static final String SINGLE_SERVER = "localhost:6381";
-	
-	//集群模式只需要写一个redis cluster模式下的服务器地址，
-	//因为redisson也和jedis一样会自动识别其他cluster模式下master和slave
-	private static final String CLUSTER_SERVER = "localhost:7000";
-	
-	private static final boolean useAnnotationListener = false;
-	
-	private final SocketIOServer server;
-	
-	private RedissonClient redisson;
-	private final PubSubStore pubSubStore;
-	
-	public SocketIORedissonServer(){
+    
+    private static final String HOST = "localhost";
+    private static final int PORT = 3001;
+    
+    //单机模式
+    private static final String SINGLE_SERVER = "localhost:6381";
+    
+    //集群模式只需要写一个redis cluster模式下的服务器地址，
+    //因为redisson也和jedis一样会自动识别其他cluster模式下master和slave
+    private static final String CLUSTER_SERVER = "localhost:7000";
+    
+    private static final boolean useAnnotationListener = false;
+    
+    private final SocketIOServer server;
+    
+    private RedissonClient redisson;
+    private final PubSubStore pubSubStore;
+    
+    public SocketIORedissonServer(){
         server = new SocketIOServer(config());
         pubSubStore = server.getConfiguration().getStoreFactory().pubSubStore();
         
@@ -53,14 +53,14 @@ public class SocketIORedissonServer implements ConnectListener, DisconnectListen
                 System.out.println("onMessage3: " + message.getNodeId() + ", " + message.getSessionId());
             }
         }, ConnectMessage.class);
-	}
-	
-	private Configuration config(){
-	    Configuration socketIOConfig = new Configuration();
-	    socketIOConfig.setHostname(HOST);
-	    socketIOConfig.setPort(PORT);
-	    socketIOConfig.setMaxFramePayloadLength(1024 * 1024);
-	    socketIOConfig.setMaxHttpContentLength(1024 * 1024);
+    }
+    
+    private Configuration config(){
+        Configuration socketIOConfig = new Configuration();
+        socketIOConfig.setHostname(HOST);
+        socketIOConfig.setPort(PORT);
+        socketIOConfig.setMaxFramePayloadLength(1024 * 1024);
+        socketIOConfig.setMaxHttpContentLength(1024 * 1024);
         
         Config redissonConfig = new Config();
         redissonConfig.useClusterServers().addNodeAddress(CLUSTER_SERVER);
@@ -81,55 +81,55 @@ public class SocketIORedissonServer implements ConnectListener, DisconnectListen
         BaseStoreFactory baseStoreFactory = new RedissonStoreFactory(redisson);
         socketIOConfig.setStoreFactory(baseStoreFactory);
         return socketIOConfig;
-	}
-	
-	public void start(){
-	    addListeners();
+    }
+    
+    public void start(){
+        addListeners();
         
         server.start();
         System.out.println("\n------ "+this.getClass().getSimpleName()+"start on "+PORT+" ------\n");
-	}
-	
-	private void addListeners(){
-	    if(useAnnotationListener){
-	        server.addListeners(new MessageAnnotation(pubSubStore));
-	    }else{
-	        server.addConnectListener(this);
-	        server.addDisconnectListener(this);
+    }
+    
+    private void addListeners(){
+        if(useAnnotationListener){
+            server.addListeners(new MessageAnnotation(pubSubStore));
+        }else{
+            server.addConnectListener(this);
+            server.addDisconnectListener(this);
 
-	        server.addEventListener("message", byte[].class, new DataListener<byte[]>() {
-	            @Override
-	            public void onData(SocketIOClient client, byte[] data, AckRequest ackRequest) {
-	                Message message = Message.parse(data);
-	                System.out.println("Received: " + message.getText());
-	                // Transform the text to upper case
-	                message.setText(message.getText().toUpperCase());
-	                // Re-encode it and send it back
-	                client.sendEvent("message", message.toByteArray());
-	                System.out.println("Sent: " + message.getText());
-	            }
-	        });
-	    }
-	}
-	
-	public void stop(){
-	    server.stop();
-	}
+            server.addEventListener("message", byte[].class, new DataListener<byte[]>() {
+                @Override
+                public void onData(SocketIOClient client, byte[] data, AckRequest ackRequest) {
+                    Message message = Message.parse(data);
+                    System.out.println("Received: " + message.getText());
+                    // Transform the text to upper case
+                    message.setText(message.getText().toUpperCase());
+                    // Re-encode it and send it back
+                    client.sendEvent("message", message.toByteArray());
+                    System.out.println("Sent: " + message.getText());
+                }
+            });
+        }
+    }
+    
+    public void stop(){
+        server.stop();
+    }
 
-	@Override
-	public void onConnect(SocketIOClient client) {
-	    String sessionId = client.getSessionId().toString();
-	    client.set("sessionId", sessionId);
-	    System.out.println(sessionId+" connecting..."+client.get("sessionId"));
-//	    RTopic<ConnectMessage> topic = redisson.getTopic("connected");
-//	    topic.publish(new ConnectMessage(client.getSessionId()));
-	    pubSubStore.publish("connected", new ConnectMessage(client.getSessionId()));
-	    
-	    //testRedissonAdd();
-	}
-	
-	private void testRedissonAdd(){
-	    List<Integer> list = redisson.getList("list");
+    @Override
+    public void onConnect(SocketIOClient client) {
+        String sessionId = client.getSessionId().toString();
+        client.set("sessionId", sessionId);
+        System.out.println(sessionId+" connecting..."+client.get("sessionId"));
+//        RTopic<ConnectMessage> topic = redisson.getTopic("connected");
+//        topic.publish(new ConnectMessage(client.getSessionId()));
+        pubSubStore.publish("connected", new ConnectMessage(client.getSessionId()));
+        
+        //testRedissonAdd();
+    }
+    
+    private void testRedissonAdd(){
+        List<Integer> list = redisson.getList("list");
         list.add(1);
         list.add(2);
         list.add(3);
@@ -138,29 +138,29 @@ public class SocketIORedissonServer implements ConnectListener, DisconnectListen
         Message message = new Message("text_"+new Random().nextInt());
         List<Message> msgList = redisson.getList("msgList");
         msgList.add(message);
-	}
-	
-	@Override
-    public void onMessage(ConnectMessage message) {
-	    System.out.println("onMessage2: "+message.getNodeId()+", "+message.getSessionId());
     }
-	
-	@Override
-	public void onDisconnect(SocketIOClient client) {
-	    String sessionId = client.getSessionId().toString();
-		System.out.println(sessionId+" disconnecting..."+client.get("sessionId"));
-		client.del("sessionId");
-		
-		//testRedissonFind();
-	}
-	
-	private void testRedissonFind(){
-	    System.out.println(Arrays.toString(redisson.getList("list").toArray()));
+    
+    @Override
+    public void onMessage(ConnectMessage message) {
+        System.out.println("onMessage2: "+message.getNodeId()+", "+message.getSessionId());
+    }
+    
+    @Override
+    public void onDisconnect(SocketIOClient client) {
+        String sessionId = client.getSessionId().toString();
+        System.out.println(sessionId+" disconnecting..."+client.get("sessionId"));
+        client.del("sessionId");
+        
+        //testRedissonFind();
+    }
+    
+    private void testRedissonFind(){
+        System.out.println(Arrays.toString(redisson.getList("list").toArray()));
         System.out.println(Arrays.toString(redisson.getList("msgList").toArray()));
-	}
-	
-	public static void main(String[] args){
-    	new SocketIORedissonServer().start();
+    }
+    
+    public static void main(String[] args){
+        new SocketIORedissonServer().start();
     }
 
 }
