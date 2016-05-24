2769d57f662a88af3fa18b9bf66c285f31c7b636
whg <whg3.3.3@163.com>
2016-05-24 16:04:29 +0800

    change tag to 4 space

--- backend/java/src/main/java/SocketIOStringServer.java
+++ backend/java/src/main/java/SocketIOStringServer.java
@@ -9,18 +9,18 @@ import com.corundumstudio.socketio.listener.DisconnectListener;
 /**
  * 与{@link SocketIOProtoServer}类似，这里为了测试就传递简单的String类型
  */
-public class SocketIOStringServer implements ConnectListener, DisconnectListener{
-    
+public class SocketIOStringServer implements ConnectListener, DisconnectListener {
+
     private static final String HOST = "localhost";
     private static final int PORT = 3001;
-    
+
     private final SocketIOServer server;
-    
-    public SocketIOStringServer(){
+
+    public SocketIOStringServer() {
         server = new SocketIOServer(config());
     }
-    
-    private Configuration config(){
+
+    private Configuration config() {
         Configuration config = new Configuration();
         config.setHostname(HOST);
         config.setPort(PORT);
@@ -28,43 +28,43 @@ public class SocketIOStringServer implements ConnectListener, DisconnectListener
         config.setMaxHttpContentLength(1024 * 1024);
         return config;
     }
-    
-    public void start(){
+
+    public void start() {
         server.addConnectListener(this);
         server.addDisconnectListener(this);
-        
+
         server.addEventListener("message", String.class, new DataListener<String>() {
             @Override
             public void onData(SocketIOClient client, String data, AckRequest ackRequest) {
                 Message message = new Message(data);
-                System.out.println("Received: "+message.getText());
+                System.out.println("Received: " + message.getText());
                 // Transform the text to upper case
                 message.setText(message.getText().toUpperCase());
                 // Re-encode it and send it back
                 client.sendEvent("message", message.getText());
-                System.out.println("Sent: "+message.getText());
+                System.out.println("Sent: " + message.getText());
             }
         });
-        
+
         server.start();
-        System.out.println("\n------ "+this.getClass().getSimpleName()+"start on "+PORT+" ------\n");
+        System.out.println("\n------ " + this.getClass().getSimpleName() + "start on " + PORT + " ------\n");
     }
-    
-    public void stop(){
+
+    public void stop() {
         server.stop();
     }
 
     @Override
     public void onConnect(SocketIOClient client) {
-        System.out.println(client.getSessionId()+" connecting..."+server.getAllClients().size());
+        System.out.println(client.getSessionId() + " connecting..." + server.getAllClients().size());
     }
-    
+
     @Override
     public void onDisconnect(SocketIOClient client) {
-        System.out.println(client.getSessionId()+" disconnecting..."+server.getAllClients().size());
+        System.out.println(client.getSessionId() + " disconnecting..." + server.getAllClients().size());
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         new SocketIOStringServer().start();
     }
 
